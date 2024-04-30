5499b6ba3cd49591bfe5ebe9b6822d785f802b82
Tanvir Rudro <rudro987@gmail.com>
2024-04-30 22:30:06 +0600

    Basic types

---
+++ module1/src/basicDataTypes.ts
@@ -0,0 +1,30 @@
+//string
+let firstName: string = 'mezba';
+
+//number
+let roll: number = 123;
+
+//boolean
+let isAdmin: boolean = true;
+
+//undefined
+
+let x: undefined = undefined;
+
+//null
+
+let y: null = null;
+
+let d; //any type
+
+//Array
+
+let friends: string[] = ['Ross', 'Joey'];
+
+let rollList: number[] = [1,2,3];
+
+// tuple ---> array --> order ...> can define type of values
+
+let coordinates: [number, number] = [1,5];
+
+let ageName: [number, string, boolean] = [30, 'Ross', true];
